bfeb8f71dc955ea6ce19a43644414e3d35ce8435
Sharon Woo <sharon.woo.2017@mitb.smu.edu.sg>
2022-03-30 12:10:17 +0800

    LoginRequiredMixin for add cheese form

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Cheese
 
@@ -10,6 +11,8 @@ class CheeseDetailView(DetailView):
     model = Cheese
 
 
-class CheeseCreateView(CreateView):  # remember to define get_absolute_url in model
+class CheeseCreateView(
+    LoginRequiredMixin, CreateView
+):  # remember to define get_absolute_url in model
     model = Cheese
     fields = ["name", "description", "firmness", "country_of_origin"]
