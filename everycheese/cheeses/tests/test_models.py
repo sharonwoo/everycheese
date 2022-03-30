d3cf88ee48bd60d6960f4b2a4d786794d67d1363
Sharon Woo <sharon.woo.2017@mitb.smu.edu.sg>
2022-03-30 13:03:55 +0800

    Add test for get_absolute_url()

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -1,20 +1,25 @@
 import pytest
-from ..models import Cheese # import cheese model class for testing
+from ..models import Cheese  # import cheese model class for testing
 from .factories import CheeseFactory
 
 pytestmark = pytest.mark.django_db
 
+
 def test___str__():
     cheese = CheeseFactory()
-    assert cheese.__str__() == cheese.name 
+
+    # test get_absolute_url
+    url = cheese.get_absolute_url()
+    assert url == f"/cheeses/{cheese.slug}/"
+
+    # test str
+    assert cheese.__str__() == cheese.name
     assert str(cheese) == cheese.name
 
-
     # cheese = Cheese.objects.create(
-    #                     name="Stracchino", 
+    #                     name="Stracchino",
     #                     description="Semi-sweet cheese that goes well with starches.",
     #                     firmness=Cheese.Firmness.SOFT,
-    #                     ) 
-    # assert cheese.__str__() == "Stracchino" 
+    #                     )
+    # assert cheese.__str__() == "Stracchino"
     # assert str(cheese) == "Stracchino"
-    
